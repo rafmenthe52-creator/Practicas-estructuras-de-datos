27b62d5ee119faa56619bbcdde367f7535b8c37c
PabloMina <pablo.minna@estudiante.uam.es>
2026-02-26 10:39:12 +0100

    p2_e1

--- p2_e1.c
+++ p2_e1.c
@@ -7,80 +7,98 @@
 #include "radio.h"
 #include "stack.h"
 
-int show_player_menu(Stack *history);
-int show_player_menu(Stack *history) {
-    int option;
-    Music *m = NULL;
-	
+int show_player_menu(Stack* history);
+int show_player_menu(Stack* history) {
+	int option;
+	Music* m = NULL;
+
 	/* Obtenemos canción actual del top de la pila */
-	m = (Music *)stack_top (history);
-    if (m != NULL) {
+	m = (Music*)stack_top(history);
+	if (m != NULL) {
 		music_formatted_print(stdout, m);
-    } else {
-        printf("\nNo song currently playing.\n");
-    }
-	
+	} else {
+		printf("\nNo song currently playing.\n");
+	}
+
 	/* Imprimimos historial (pila) */
 	printf("\nRecently Played:\n");
 	stack_print(stdout, history, music_plain_print);
-	
+
 	/* Mostramos menu y esperamos selección */
-    printf("\n1. Back to previous song\n");
-    printf("2. Exit\n");
-    printf("Choose an option: ");
+	printf("\n1. Back to previous song\n");
+	printf("2. Exit\n");
+	printf("Choose an option: ");
 
-    scanf("%d", &option);
+	scanf("%d", &option);
 
-    return option;
+	return option;
 }
 
+void exit_execution(Radio* radio) {}
+
 /* TODO MAIN FUNCTION */
-int main (int argc, char **argv) {
-  FILE* f;
-  Radio* r;
-  Stack* s;
-  Music **list;
-  int n, i;
+int main(int argc, char** argv) {
+	FILE* f;
+	Radio* r;
+	Stack* s;
+	Music** list;
+	int n, i;
 
-  if (argc != 2) {
-    return -1;
-  }
+	if (argc != 2) {
+		return -1;
+	}
 
-  f = fopen(argv[1], "r");
-  if (f == NULL) {
-    fprintf(stderr, "Error al leer el fichero de texto");
-    return -1;
-  }
+	f = fopen(argv[1], "r");
+	if (f == NULL) {
+		fprintf(stderr, "Error al leer el fichero de texto");
+		return -1;
+	}
 
-  r = radio_init();
+	r = radio_init();
 
-  if (radio_readFromFile(f, r) == ERROR) {
-    return -1;
-  } 
+	if (radio_readFromFile(f, r) == ERROR) {
+		return -1;
+	}
 
-  s=stack_init();
+	s = stack_init();
 
-  list=radio_getMusicList(r);
+	list = radio_getMusicList(r);
 
-  n=radio_getNumberOfMusic(r);
+	n = radio_getNumberOfMusic(r);
 
-  for(i=0, i<n, i++){
-    stack_push(s, list[i]);
-  }
-  
-  music_formatted_print(stack_top(s));
+	for (i = 0; i < n; i++) {
+		stack_push(s, list[i]);
+	}
 
-  if(!list){
-    fprintf(stdout, "No song currently playing.");
-  }else{
-    fprintf(stdout, "Recently played: \n SIZE: %d", stack_size(s));
+	music_formatted_print(stdout, stack_top(s));
 
-    for(i=stack_size(s); i>0; i--){
-      music_plain_print(list[i]);
-    }
-  }
+	if (!list) {
+		fprintf(stdout, "No song currently playing.");
+	} else {
+		fprintf(stdout, "Recently played: \n SIZE: %d", stack_size(s));
 
-  free(list);
+		for (i = stack_size(s); i > 0; i--) {
+			music_plain_print(stdout, list[i]);
+		}
+	}
+
+	while (stack_isEmpty == FALSE && show_player_menu != 2) {
+		switch (show_player_menu(s)) {
+		case 1:
+			stack_pop(s);
+			break;
+		case 2:
+			exit_execution(r);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	free(list);
+
+	return 0;
 }
 
 
