3a94474767566567dcd02e61cd7db32082824a72
Rafael Velasco Wagner <rafmenthe52@gmail.com>
2026-03-04 12:21:43 +0100

    mergestack

---
+++ mergestacks.c
@@ -0,0 +1,22 @@
+/**
+* @brief: Merges two stacks
+*
+* @param sin1, first input stack
+* @param sin2, second input stack
+* @param sout, result stack
+* @return The function returns OK or ERROR
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "music.h"
+#include "stack.h"
+
+Status mergeStacks (Stack *sin1, Stack *sin2, Stack *sout){
+  while(stack_isEmpty(sin1)==FALSE && stack_isEmpty(sin2)==FALSE){
+    if(music_getDuration(stack_top(sin1))>music_getDuration(stack_top(sin2))){
+        stack_pop(sin1)
+    }
+  }
+}
